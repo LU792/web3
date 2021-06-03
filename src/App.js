42bd7c2e6582e3e0627f671c36980bdb25743bd2
LU792 <55209387+LU792@users.noreply.github.com>
2021-06-03 11:47:14 -0300

    as
    as

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import Routes from './router';
 import '../src/style/pages/global.css';
+
+import './App.css';
+
+const express = require('express');
+const app = express();
+const employeeRouters = require('./routes/employeeRoute')
+//Route
+app.use('/employee',employeeRouters)
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(express.json());
+
+app.use('/',(req,res)=>{
+  res.send("Hello World form NodeJS express.");
+});
+
+
+app.listen(app.get('port'),()=>{
+  console.log("Start server on port "+app.get('port'))
+
+
 function App() {
   return (
 <Routes/>
@@ -8,6 +31,6 @@ function App() {
   )
   
 }
-
+})
 
 export default App;
